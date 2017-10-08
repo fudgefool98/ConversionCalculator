6eab29a19bf5ce6722a385ce44422f50c5e2b813
Brian Riddle <bar6x2@atm0561136dyn.dhcp.missouri.edu>
2017-10-08 16:47:42 -0500

    Added conversion requirement comments

--- Conversion Calculator/model.swift
+++ Conversion Calculator/model.swift
@@ -7,3 +7,68 @@
 //
 
 import Foundation
+
+//    -length
+//
+//    Kilometer (km) to meter (m)
+//
+//    Kilometer(km) to decimeter (dm)
+//
+//    Kilometer (km) to centimeter (cm)
+//
+//    Kilometer (km) to millimeter (mm)
+//
+//    Kilometer (km) to mile (mile)
+//
+//    Kilometer (km) to yard (yard)
+//
+//    Kilometer (km) to feet (ft)
+//
+//    Kilometer (km) to inch (in)
+//
+//    -speed
+//
+//    Kilometer per Hour (km/h) to Meter per second (m/s)
+//
+//    Kilometer per Hour (km/h) to Mile per Hour (mph)
+//
+//    Kilometer per Hour (km/h) to Feet per Minute (ft/min)
+//
+//    Kilometer per Hour (km/h) to Feet per Second (ft/s)
+//
+//    -volume
+//
+//    Cubic Meter(m3) to Deciliter (dl)
+//
+//    Cubic Meter (m3) to Centiliter (cl)
+//
+//    Cubic Meter (m3) to Mililiter (cc)
+//
+//    Cubic Meter (m3) to Cubic Millimeter (mm3)
+//
+//    Cubic Meter (m3) to Cubic Yard (yd3)
+//
+//    Cubic Meter (m3) to Cubic Foot (ft3)
+//
+//    Cubic Meter (m3) to Cubic Inch (in3)
+//
+//    -temp
+//
+//    Celsius © to Fahrenheit (F)
+//
+//    Celsius © to Kelvin (K)
+//
+//    -weight
+//
+//    Kilogram (kg) to Gram (gr)
+//
+//    Kilogram (kg) to Milligram (mg)
+//
+//    Kilogram (kg) to Pound (lb)
+//
+//    Kilogram (kg) to Ounce (oz)
+//
+//
+//
+//
+//
