078f8d525b617d7bf595e746adcb4b269a165469
fudgefool98 <jordanliebman@mwc-054064.dhcp.missouri.edu>
2017-10-08 17:38:58 -0500

    added various formuli

--- Conversion Calculator/model.swift
+++ Conversion Calculator/model.swift
@@ -8,67 +8,93 @@
 
 import Foundation
 
-//    -length
-//
-//    Kilometer (km) to meter (m)
-//
-//    Kilometer(km) to decimeter (dm)
-//
-//    Kilometer (km) to centimeter (cm)
-//
-//    Kilometer (km) to millimeter (mm)
-//
-//    Kilometer (km) to mile (mile)
-//
-//    Kilometer (km) to yard (yard)
-//
-//    Kilometer (km) to feet (ft)
-//
-//    Kilometer (km) to inch (in)
-//
-//    -speed
-//
-//    Kilometer per Hour (km/h) to Meter per second (m/s)
-//
-//    Kilometer per Hour (km/h) to Mile per Hour (mph)
-//
-//    Kilometer per Hour (km/h) to Feet per Minute (ft/min)
-//
-//    Kilometer per Hour (km/h) to Feet per Second (ft/s)
-//
-//    -volume
-//
-//    Cubic Meter(m3) to Deciliter (dl)
-//
-//    Cubic Meter (m3) to Centiliter (cl)
-//
-//    Cubic Meter (m3) to Mililiter (cc)
-//
-//    Cubic Meter (m3) to Cubic Millimeter (mm3)
-//
-//    Cubic Meter (m3) to Cubic Yard (yd3)
-//
-//    Cubic Meter (m3) to Cubic Foot (ft3)
-//
-//    Cubic Meter (m3) to Cubic Inch (in3)
-//
-//    -temp
-//
-//    Celsius © to Fahrenheit (F)
-//
-//    Celsius © to Kelvin (K)
-//
-//    -weight
-//
-//    Kilogram (kg) to Gram (gr)
-//
-//    Kilogram (kg) to Milligram (mg)
-//
-//    Kilogram (kg) to Pound (lb)
-//
-//    Kilogram (kg) to Ounce (oz)
 //
 //
+//-length
 //
-// test 
+//Kilometer (km) to meter (m)
+//  output = input*1000
+
+//Kilometer(km) to decimeter (dm)
+// output = input*100
+
+//Kilometer (km) to centimeter (cm)
+// ouput = input*100000
+
+//Kilometer (km) to millimeter (mm)
+// output = input * 1000000
+
+//Kilometer (km) to mile (mile)
+// output = input * 0.621371
+
+//Kilometer (km) to yard (yard)
+// output = input * 1093.61
+
+//Kilometer (km) to feet (ft)
+// output = input * 3280.84
+
+//Kilometer (km) to inch (in)
+// output = input * 39370.1
+
+
+
+//-speed
 //
+//Kilometer per Hour (km/h) to Meter per second (m/s)
+// output = input *0.277778
+
+//Kilometer per Hour (km/h) to Mile per Hour (mph)
+// output = input *0.621371
+
+//Kilometer per Hour (km/h) to Feet per Second (ft/s)
+// output = input *0.911344
+
+
+//-volume
+//
+//Cubic Meter(m3) to Deciliter (dl)
+//
+
+//Cubic Meter (m3) to Centiliter (cl)
+//
+
+//Cubic Meter (m3) to Mililiter (cc)
+//
+
+//Cubic Meter (m3) to Cubic Millimeter (mm3)
+//
+
+//Cubic Meter (m3) to Cubic Yard (yd3)
+//
+
+//Cubic Meter (m3) to Cubic Foot (ft3)
+//
+
+//Cubic Meter (m3) to Cubic Inch (in3)
+//
+
+//-temp
+//
+//Celsius © to Fahrenheit (F)
+//
+
+//Celsius © to Kelvin (K)
+//
+
+
+//-weight
+//
+//Kilogram (kg) to Gram (gr)
+//
+
+//Kilogram (kg) to Milligram (mg)
+//
+
+//Kilogram (kg) to Pound (lb)
+//
+
+//Kilogram (kg) to Ounce (oz)
+//
+//
+//
+
