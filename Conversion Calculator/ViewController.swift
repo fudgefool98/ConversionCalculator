cb584f401f24d9a5bfae92898af4c5b3383a9606
Brian Riddle <bar6x2@Brians-MacBook-Pro.local>
2017-10-09 22:23:39 -0500

    Button functionality for input completed

--- Conversion Calculator/ViewController.swift
+++ Conversion Calculator/ViewController.swift
@@ -9,14 +9,46 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var inputField: UITextField!
+    
+    @IBAction func onNumClick(_ sender: UIButton) {
+       
+        if(inputField.text == "Input"){
+            
+            inputField.text = ""
+        }
+        if (sender.titleLabel!.text != "." || !inputField.text!.contains(".")) {
+        inputField.text! += sender.titleLabel!.text!;
+        }
+    }
+   
+    @IBAction func makePositive(_ sender: Any) {
+        if (inputField.text!.starts(with: "-")) {
+            inputField.text!.remove(at: inputField.text!.startIndex)
+        }
+    }
+    
+    @IBAction func makeNegative(_ sender: Any) {
+        if (!inputField.text!.starts(with: "-")) {
+            inputField.text = "-" + inputField.text!
+        }
+    }
+    
+    
+    
+    @IBAction func acButton(_ sender: Any) {
+        if (inputField.text!.count > 0) {
+            inputField.text!.remove(at: inputField.text!.index(before: inputField.text!.endIndex))
+        }
+    }
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
